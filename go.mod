ddcab8c5d870bc8a20418cdd0b4753c76a8c4218
evan <281406674@qq.com>
2018-12-17 14:38:24 +0800

    测试文件目录持续写入
    Signed-off-by: evan <281406674@qq.com>

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/sanguohot/dcm-timer
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/google/uuid v1.1.0
 	github.com/pkg/errors v0.8.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/viper v1.2.1
---
+++ pkg/test/test_file_append.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sanguohot/dcm-timer/etc"
+	"github.com/sanguohot/dcm-timer/pkg/common/log"
+	"go.uber.org/zap"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+func ApendToFile(filePath string)  {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		log.Logger.Fatal(err.Error())
+	}
+	n, err := file.WriteString(fmt.Sprintf("%v\n", uuid.New()))
+	if err != nil {
+		log.Logger.Fatal(err.Error())
+	}
+	log.Sugar.Debugf("append %s, size => %d, content => %s", filePath, n, uuid.New().String())
+}
+
+func ApendToFileTask(filePath string)  {
+	ticks := time.NewTicker(time.Duration(10) * time.Millisecond)
+	tick := ticks.C
+	go func() {
+		for _ = range tick {
+			ApendToFile(filePath)
+		}
+	}()
+}
+
+func CheckDirIsStillWriting(k string) (bool) {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	isWriting := make(chan bool, 1)
+	go func() {
+		now := time.Now()
+		var (
+			maxDirSize int64 = 0
+			curDirSize int64 = 0
+		)
+		for t := range ticker.C {
+			log.Sugar.Debugf("%v 开始检查目录 %s", t, k)
+			dirInfo, err := os.Stat(k)
+			if err != nil {
+				log.Logger.Error(err.Error(), zap.String("k", k))
+				continue
+			}
+			modTime := dirInfo.ModTime()
+			log.Sugar.Debugf("目录 %s 最近修改时间 %v", k, modTime)
+			if modTime.After(now) {
+				log.Sugar.Infof("目录 %s 有写入, 修改时间更新 %d => %d,跳过拷贝", k, now.Unix(), modTime.Unix())
+				isWriting <- true
+				break
+			}
+			curDirSize = 0
+			err = filepath.Walk(k, func(path string, info os.FileInfo, err error) error {
+				if info.ModTime().After(now) {
+					log.Sugar.Infof("文件 %s => %s 有写入, 跳过拷贝", path, info.Name())
+					return errors.New("FILE_WRITING")
+				}
+				curDirSize += info.Size()
+				return nil
+			})
+			if err != nil {
+				isWriting <- true
+				break
+			}
+			if maxDirSize == 0 {
+				maxDirSize = curDirSize
+			}
+			if maxDirSize < curDirSize {
+				log.Sugar.Infof("目录 %s 有写入, 占用空间增大 %d => %d, 跳过拷贝", k, maxDirSize, curDirSize)
+				isWriting <- true
+				break
+			}
+			tenSecLater := now.Add(time.Duration(20) * time.Second)
+			if tenSecLater.Before(t) {
+				log.Sugar.Infof("%d秒内目录 %s 没有任何修改, 拟进行拷贝, %d vs %d", 20, k, tenSecLater.Unix(), t.Unix())
+				isWriting <- false
+			}
+		}
+	}()
+	result := <-isWriting
+	ticker.Stop()
+	return result
+}
+
+func CheckDir(dirPath string)  {
+	go CheckDirIsStillWriting(dirPath)
+}
+
+func main()  {
+	dirPath := path.Join(etc.GetServerDir(), "data/test/")
+	filePath := path.Join(dirPath, "test.txt")
+	ApendToFileTask(filePath)
+	CheckDir(dirPath)
+	//time.Sleep(time.Second * 10)
+
+	quit := make(chan os.Signal, 1)
+	<-quit
+	//finder.CheckDirIsStillWriting()
+}
