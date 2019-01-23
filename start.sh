e5364bb70ef38c1026dc0fbbb476d657f7156497
evan <281406674@qq.com>
2019-01-23 10:56:36 +0800

    go1.11.2不支持-d=true，改用-d true

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 export DCM_TIMER_PATH=/opt/dcm-timer
 export DCM_TIMER_TYPE=production
 #nohup /opt/dcm-timer/dcm-timer > /opt/dcm-timer/nohup.out 2>&1 &
-/opt/dcm-timer/dcm-timer -d=true
+/opt/dcm-timer/dcm-timer -d true
